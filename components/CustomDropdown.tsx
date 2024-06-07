b7a7f6fef79f067ac778e086e83366546baa9e07
Dev240204 <dev.ace21@sot.pdpu.ac.in>
2024-06-07 21:10:38 +0530

    Custom Dropdown Changed

--- components/CustomDropdown.tsx
+++ components/CustomDropdown.tsx
@@ -1,13 +1,14 @@
 import Animated, { FadeIn } from "react-native-reanimated";
-import { Button } from "~/components/ui/button";
-import { Text } from "~/components/ui/text";
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "~/components/ui/dropdown-menu";
-import { Ionicons } from "@expo/vector-icons";
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '~/components/ui/select';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import React, { useState } from "react";
 import { Separator } from "./ui/separator";
 
@@ -20,43 +21,36 @@ const CustomDropdown: React.FC<CustomDropdownProps> = ({
   title,
   itemsList,
 }) => {
-  const [open, setOpen] = useState(false);
-  const [item, setItem] = useState(title);
-
-  const handleSelectItem = (item: string) => {
-    setItem(item);
-    setOpen(false);
+  const insets = useSafeAreaInsets();
+  const contentInsets = {
+    top: insets.top,
+    bottom: insets.bottom,
+    left: 12,
+    right: 12,
   };
   return (
-    <DropdownMenu
-      className="w-full flex flex-col"
-      open={open}
-      onOpenChange={(newVal) => {
-        setOpen(newVal);
-      }}
+    <Select
+      defaultValue={{ value: title, label: title }}
     >
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="outline"
-          className="flex flex-row justify-between border-0 h-11 bg-[#f1f5f9] dark:bg-gray-800 px-4 rounded-md font-medium dark:text-gray-100"
-        >
-          <Text className="text-gray-500">{item}</Text>
-          <Ionicons name="chevron-down-outline" size={25} />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="bg-slate-200 border-0 my-1 w-96">
-        <Animated.View entering={FadeIn.duration(200)} className="divide-y-2">
+      <SelectTrigger className="w-full">
+        <SelectValue
+          className='text-foreground text-sm native:text-lg'
+          placeholder={`Select ${title}`}
+        />
+      </SelectTrigger>
+      <SelectContent insets={contentInsets} className="bg-slate-200 border-0 my-1 w-96">
+        <SelectGroup>
+          <SelectLabel>{title}</SelectLabel>
           {itemsList.map((item: string, index) => (
-            <DropdownMenuItem
-              key={index}
-              onPress={() => handleSelectItem(item)}
+            <SelectItem
+              key={index} label={item} value={item}
             >
-              <Text className="font-acumin text-sm text-gray-900">{item}</Text>
-            </DropdownMenuItem>
+              {item}
+            </SelectItem>
           ))}
-        </Animated.View>
-      </DropdownMenuContent>
-    </DropdownMenu>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
   );
 };
 
