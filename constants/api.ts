f7df562c0578b5cc22b1fcd90d4820886ec2afd2
Dhruvil <ddyhs25@gmail.com>
2025-06-14 17:14:05 +0530

    updated for android build

--- constants/api.ts
+++ constants/api.ts
@@ -5,8 +5,8 @@ export const inProduction = process.env.NODE_ENV === "production";
 //   : process.env.EXPO_PUBLIC_API_URL;
 
 // export const API_URL = "http://192.168.50.199:3000";
-// export const API_URL = "https://pgplcrm.prasadsos.co:8091";
-export const API_URL =
-  "https://d4df-2409-40c1-33-d30d-cd78-af84-5cb-d6df.ngrok-free.app";
+export const API_URL = "https://pgplcrm.prasadsos.co:8091";
+// export const API_URL =
+//   "https://d4df-2409-40c1-33-d30d-cd78-af84-5cb-d6df.ngrok-free.app";
 
 console.log("API_URL", API_URL);
