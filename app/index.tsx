043ff4b361a3aa204b42ab832816a1f0ccaa8957
Dhruvil <ddyhs25@gmail.com>
2025-06-15 00:42:15 +0530

    updated android app build

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,7 @@ import { Redirect } from "expo-router";
 import { View, Text } from "react-native";
 
 const index = () => {
-  // return <Redirect href={"/signin"} />;
-  return <Text>Home Screen</Text>;
+  return <Redirect href={"/signin"} />;
 };
 
 export default index;
